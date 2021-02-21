a42993843cf14daea02f0334a6f3d8e901ec11cb
Babajide Owosakin <babajide.owosakin@getyourguide.com>
2021-02-21 19:27:51 +0100

    chore: refactor config and add validation to env vars

---
+++ src/config/config.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const Joi = require('joi')
+
+const envVarsSchema = Joi
+    .object()
+    .keys({
+        NODE_ENV: Joi
+            .string()
+            .valid("production", "development", "test")
+            .required(),
+        DB_HOST: Joi.string().required(),
+        DB_USER: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        DB_PASS: Joi.string().required(),
+        DB_DIALECT: Joi.string().required(),
+        DB_LOGGING: Joi.boolean().default(false),
+        REDDIT_SECRET_KEY: Joi.string().required(),
+        REDDIT_APP_NAME: Joi.string().required(),
+    })
+    .unknown(true)
+
+const { value: envVars, error } = envVarsSchema
+    .prefs({ errors: { label: 'key' } })
+    .validate(process.env)
+
+if (error) {
+    throw new Error(`Config validation error: ${error.message}`);
+}
+
+module.exports = {
+    env: envVars.NODE_ENV,
+    database: {
+        host: envVars.DB_HOST,
+        user: envVars.DB_USER,
+        name: envVars.DB_NAME,
+        password: envVars.DB_PASS,
+        dialect: envVars.DB_DIALECT,
+        logging: envVars.DB_LOGGING,
+    },
+    reddit: {
+        secretKey: envVars.REDDIT_SECRET_KEY,
+        appName: envVars.REDDIT_APP_NAME,
+    }
+}
+
--- src/infrastructure/database.js
+++ src/infrastructure/database.js
@@ -3,16 +3,10 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const { database } = require('../config/config')
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(database.name, database.user, database.password, database);
 
 const files = []
 const sortDir = (mainDir) => {
